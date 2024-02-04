5e346c8d7d49c813b1b9a5a7078cb15a252993af
User <you@example.com>
2024-02-04 23:59:10 +0100

    added code

--- automaticBike/src/autoBike/AutoBike.java
+++ automaticBike/src/autoBike/AutoBike.java
@@ -1,2 +1,50 @@
-package autoBike;public class AutoBike {
+package autoBike;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AutoBike {
+    private boolean isOn;
+    private int currentGear;
+    private int speed;
+
+    public boolean isOn() {
+        return isOn;
+    }
+    public void toggleSwitch() {
+        isOn = !isOn;
+    }
+
+    public void accelerate() {
+       speed++;
+    }
+    public int getSpeed(){
+        return speed;
+    }
+    public void getGears() {
+        int gearRange = 4;
+        for (currentGear = 1; currentGear <= gearRange; currentGear++) {
+            if (currentGear == 1) {
+                speed++;
+            } else if (currentGear == 2) {
+                speed += 2;
+
+            } else if (currentGear == 3) {
+                speed += 3;
+            } else if (currentGear == 4) {
+                speed += 4;
+            }
+        }
+        // return speed();
+//    }
+//  public void decelerate(){
+//    accelerate--;
+//
+//}
+//
+//
+//    public int speed(){
+//        speed++;
+//        return speed;
+//    }
+    }
 }
--- automaticBike/src/autoBike/AutoBikeTest.java
+++ automaticBike/src/autoBike/AutoBikeTest.java
@@ -1,4 +1,48 @@
+package autoBike;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 class AutoBikeTest {
-  
-}
+    @Test
+    public void autoBikeIsOn_autoBikeIsOnTest(){
+        AutoBike autoBike = new AutoBike();
+        assertFalse(autoBike.isOn());
+        autoBike.toggleSwitch();
+        assertTrue(autoBike.isOn());
+    }
+    @Test
+    public void autoBikeIsOff_autoBikeIsOffTest() {
+        AutoBike autoBike = new AutoBike();
+        assertFalse(autoBike.isOn());
+
+        autoBike.toggleSwitch();
+        assertTrue(autoBike.isOn());
+        autoBike.toggleSwitch();
+        assertFalse(autoBike.isOn());
+    }
+
+
+      @Test
+    public void autoBikeCanAccelerate_autoBikeCanAccelerateTest() {
+          AutoBike autoBike = new AutoBike();
+          assertFalse(autoBike.isOn());
+          autoBike.toggleSwitch();
+          assertTrue(autoBike.isOn());
+          autoBike.accelerate();
+          assertEquals(1,autoBike.getSpeed());
+
+          }
+      }
+
+
+//   @Test
+//    public void autoBikeCanDeccelerate_autoBikeCanDeccelerateTest(){
+//        AutoBike autoBike = new AutoBike();
+//        assertFalse(autoBike.isOn());
+//        autoBike.toggleSwitch();
+//        assertTrue(autoBike.isOn());
+//        autoBike.accelerate();
+//        assertEquals(2, autoBike.accelerate());
+//        autoBike.deccelerate();
+//        assertEquals(0,autoBike.deccelerate());
+//    }
+
