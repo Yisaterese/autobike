afa37561dbeeb7744485e0946bce3b32340a52a8
Yisaterese <teresejosephyisa@gmail.com>
2024-02-05 22:07:34 +0100

    added code

--- automaticBike/src/autoBike/AutoBikeTest.java
+++ automaticBike/src/autoBike/AutoBikeTest.java
@@ -88,6 +88,22 @@ class AutoBikeTest {
     }
 
     @Test
+    public void autoBikeCanDecelerateAtGearTwoSpeed16_autoBikeCanDecelerateAtGearTwoSpeed15Test() {
+        AutoBike autoBike = new AutoBike();
+        assertFalse(autoBike.isOn());
+        autoBike.toggleSwitch();
+        assertTrue(autoBike.isOn());
+        autoBike.accelerate();
+        for (int count = 1; count <= 21; count++) {
+            autoBike.accelerate();
+        }
+        assertEquals(23, autoBike.getSpeed());
+
+        autoBike.decelerate();
+        assertEquals(21, autoBike.getSpeed());
+
+    }
+        @Test
     public void autoBikeCanDecelerateAtGearThreeSpeed47_autoBikeCanDecelerateAtGearThreeSpeed43Test() {
         AutoBike autoBike = new AutoBike();
         assertFalse(autoBike.isOn());
