3652787eacebb2b8f37ac3e5ac2bef7872c0a380
Yisaterese <teresejosephyisa@gmail.com>
2024-02-05 19:43:33 +0100

    added code

--- automaticBike/src/autoBike/AutoBikeTest.java
+++ automaticBike/src/autoBike/AutoBikeTest.java
@@ -58,6 +58,7 @@ class AutoBikeTest {
         }
         assertEquals(47, autoBike.getSpeed());
     }
+
     @Test
     public void autoBikeCanAccelerateAtGearFourSpeed41_autoBikeCanAccelerateAtGearFourSpeedTest() {
         AutoBike autoBike = new AutoBike();
